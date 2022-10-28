69565d93148f0c7a9af52ea8e0339779de92f7b5
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-10-28 13:07:31 +0700

    feat: add custom color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     extend: {
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif']
+      },
+      colors: {
+        'custom-red': '#ED4C5C',
+        'custom-yellow': '#FFCE31',
+        'custom-green': '#00A790',
+        'custom-purple': '#B01AFF',
+        'custom-blue': '#16ABF8',
+        'custom-bg-gray': '#E5E5E5',
+        'custom-icon-gray': '#A4A4A4',
+        'custom-button-gray': '#F4F4F4',
+        'custom-checkbox-gray': '#C7C7C7',
+        'custom-black': '#111111',
       }
     },
   },
