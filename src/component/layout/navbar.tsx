0a4450e2fc5ec401a88c820190606afc3ec5b395
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-10-28 14:29:36 +0700

    feat: add styling navbar

--- src/component/layout/navbar.tsx
+++ src/component/layout/navbar.tsx
@@ -1,3 +1,9 @@
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="bg-custom-blue px-5 flex justify-center">
+      <h1 className="py-7 text-white max-w-5xl w-full font-bold text-xl">
+        To Do List App
+      </h1>
+    </header>
+  );
 }
