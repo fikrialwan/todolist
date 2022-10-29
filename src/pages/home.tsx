68f698f45e5b7da2f9fff22e19e833d45c43eed1
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-10-29 07:00:50 +0700

    feat: add styling activy empty state

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,20 @@
 import { AddButton } from "../component/ui";
+import ActivityEmptyIllustration from "./../assets/illustrations/activity-empty-state.svg";
 
 export const Home = () => {
   return (
-    <div>
-      Home Page <AddButton onClick={() => {}} />
+    <div className="flex flex-col gap-10">
+      <div className="flex justify-between items-center">
+        <h1 className="text-custom-black font-bold text-4xl">Activity</h1>
+        <AddButton onClick={() => {}} />
+      </div>
+      <div className="w-full flex-1">
+        <img
+          src={ActivityEmptyIllustration}
+          alt="Activity Empty Illustration"
+          className="object-contain object-top max-w-2xl w-full mx-auto"
+        />
+      </div>
     </div>
   );
 };
